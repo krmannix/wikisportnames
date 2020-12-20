2e3eba73ccb3f4f7cf7cac70b33818dae07ad8b3
K. Rodman Mannix <kevinrmannix@gmail.com>
2020-12-20 13:43:13 -0500

    lib/wikisportnames/mlb_hall_of_fame.ex

--- lib/wikisportnames.ex
+++ lib/wikisportnames.ex
@@ -1,18 +1,2 @@
 defmodule Wikisportnames do
-  @moduledoc """
-  Documentation for `Wikisportnames`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Wikisportnames.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
---
+++ lib/wikisportnames/mlb_hall_of_fame.ex
@@ -0,0 +1,36 @@
+defmodule MlbHallOfFame do
+  import Meeseeks.XPath
+
+  @root "https://en.wikipedia.org"
+
+  def run do
+    make_request()
+    |> get_response_body
+    |> parse_names
+  end
+
+  defp make_request do
+    path = @root <> "/wiki/List_of_members_of_the_Baseball_Hall_of_Fame"
+    HTTPoison.get!(path)
+  end
+
+  defp get_response_body(response) do
+    response.body
+  end
+
+  defp parse_names(body) do
+    fetch_rows(body)
+  end
+
+  defp fetch_rows(body) do
+    italicized_nodes = Meeseeks.parse(body)
+    |> Meeseeks.all(xpath("//*[@id=\"mw-content-text\"]/div[1]/table[3]/tbody/tr/td[2]/i/span/span/span/a"))
+    |> Enum.map(fn x -> Meeseeks.text(x) end)
+
+    nonitalicized_nodes = Meeseeks.parse(body)
+    |> Meeseeks.all(xpath("//*[@id=\"mw-content-text\"]/div[1]/table[3]/tbody/tr/td[2]/span/span/span/a"))
+    |> Enum.map(fn x -> Meeseeks.text(x) end)
+
+    italicized_nodes ++ nonitalicized_nodes
+  end
+end
